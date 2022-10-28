4a63a3b7a3245e23da7b2db5a12f8b0ca5f45a60
dilwong <dilwong@gmail.com>
2022-10-27 21:41:50 -0400

    Added MATBG STM device figure

--- Twisted Bilayer Graphene Device/blender moire.py
+++ Twisted Bilayer Graphene Device/blender moire.py
@@ -21,9 +21,9 @@ def bravais_lattice(n1, n2, height):
 
 mesh = bpy.data.meshes.new('mesh')
 graph = bpy.data.objects.new('graph', mesh)
-bpy.context.scene.objects.link(graph)
-bpy.context.scene.objects.active = graph
-graph.select = True
+bpy.context.collection.objects.link(graph)
+bpy.context.view_layer.objects.active = graph
+bpy.context.active_object.select_set(state=True)
 mesh = bpy.context.object.data
 bm = bmesh.new()
 
@@ -35,33 +35,33 @@ bm.to_mesh(mesh)
 bm.free()
 
 mat=bpy.data.materials.new('Carbon')
-mat.diffuse_color=(0,0,0)
-bpy.ops.mesh.primitive_uv_sphere_add(location=(0,0,0), size = .3, ring_count = 48, segments = 48)
+mat.diffuse_color=(0,0,0,1)
+bpy.ops.mesh.primitive_uv_sphere_add(location=(0,0,0), radius = .3, ring_count = 48, segments = 48)
 sphere=bpy.context.object
 bpy.context.active_object.name = 'Carbon Atom A'
 sphere.data.materials.append(mat)
 bpy.data.objects['Carbon Atom A'].parent = bpy.data.objects['graph']
-bpy.ops.mesh.primitive_uv_sphere_add(location=(0,-a,0), size = .3, ring_count = 48, segments = 48)
+bpy.ops.mesh.primitive_uv_sphere_add(location=(0,-a,0), radius = .3, ring_count = 48, segments = 48)
 sphere=bpy.context.object
 bpy.context.active_object.name = 'Carbon Atom B'
 sphere.data.materials.append(mat)
 bpy.data.objects['Carbon Atom B'].parent = bpy.data.objects['graph']
-bpy.ops.mesh.primitive_uv_sphere_add(location=(np.sqrt(3)*a/2,-3*a/2,0), size = .3, ring_count = 48, segments = 48)
+bpy.ops.mesh.primitive_uv_sphere_add(location=(np.sqrt(3)*a/2,-3*a/2,0), radius = .3, ring_count = 48, segments = 48)
 sphere=bpy.context.object
 bpy.context.active_object.name = 'Carbon Atom C'
 sphere.data.materials.append(mat)
 bpy.data.objects['Carbon Atom C'].parent = bpy.data.objects['graph']
-bpy.ops.mesh.primitive_uv_sphere_add(location=(np.sqrt(3)*a,-a,0), size = .3, ring_count = 48, segments = 48)
+bpy.ops.mesh.primitive_uv_sphere_add(location=(np.sqrt(3)*a,-a,0), radius = .3, ring_count = 48, segments = 48)
 sphere=bpy.context.object
 bpy.context.active_object.name = 'Carbon Atom D'
 sphere.data.materials.append(mat)
 bpy.data.objects['Carbon Atom D'].parent = bpy.data.objects['graph']
-bpy.ops.mesh.primitive_uv_sphere_add(location=(np.sqrt(3)*a,0,0), size = .3, ring_count = 48, segments = 48)
+bpy.ops.mesh.primitive_uv_sphere_add(location=(np.sqrt(3)*a,0,0), radius = .3, ring_count = 48, segments = 48)
 sphere=bpy.context.object
 bpy.context.active_object.name = 'Carbon Atom E'
 sphere.data.materials.append(mat)
 bpy.data.objects['Carbon Atom E'].parent = bpy.data.objects['graph']
-bpy.ops.mesh.primitive_uv_sphere_add(location=(np.sqrt(3)*a/2,a/2,0), size = .3, ring_count = 48, segments = 48)
+bpy.ops.mesh.primitive_uv_sphere_add(location=(np.sqrt(3)*a/2,a/2,0), radius = .3, ring_count = 48, segments = 48)
 sphere=bpy.context.object
 bpy.context.active_object.name = 'Carbon Atom F'
 sphere.data.materials.append(mat)
@@ -98,15 +98,15 @@ bpy.data.objects['Bond 4'].parent = bpy.data.objects['graph']
 bpy.data.objects['Bond 5'].parent = bpy.data.objects['graph']
 bpy.data.objects['Bond 6'].parent = bpy.data.objects['graph']
 
-bpy.data.objects['graph'].dupli_type = 'VERTS'
+bpy.data.objects['graph'].instance_type = 'VERTS'
 
 #######################
 
 mesh_lower = bpy.data.meshes.new('mesh_lower')
 graph_lower = bpy.data.objects.new('graph_lower', mesh_lower)
-bpy.context.scene.objects.link(graph_lower)
-bpy.context.scene.objects.active = graph_lower
-graph_lower.select = True
+bpy.context.collection.objects.link(graph_lower)
+bpy.context.view_layer.objects.active = graph_lower
+bpy.context.active_object.select_set(state=True)
 mesh_lower = bpy.context.object.data
 bm_lower = bmesh.new()
 
@@ -117,32 +117,32 @@ for site in bravais_lattice(20,20,0):
 bm_lower.to_mesh(mesh_lower)
 bm_lower.free()
 
-bpy.ops.mesh.primitive_uv_sphere_add(location=(0,0,0), size = .3, ring_count = 48, segments = 48)
+bpy.ops.mesh.primitive_uv_sphere_add(location=(0,0,0), radius = .3, ring_count = 48, segments = 48)
 sphere=bpy.context.object
 bpy.context.active_object.name = 'Carbon Atom A Lower
 sphere.data.materials.append(mat)
 bpy.data.objects['Carbon Atom A Lower'].parent = bpy.data.objects['graph_lower']
-bpy.ops.mesh.primitive_uv_sphere_add(location=R.dot((0,-a,0)), size = .3, ring_count = 48, segments = 48)
+bpy.ops.mesh.primitive_uv_sphere_add(location=R.dot((0,-a,0)), radius = .3, ring_count = 48, segments = 48)
 sphere=bpy.context.object
 bpy.context.active_object.name = 'Carbon Atom B Lower'
 sphere.data.materials.append(mat)
 bpy.data.objects['Carbon Atom B Lower'].parent = bpy.data.objects['graph_lower']
-bpy.ops.mesh.primitive_uv_sphere_add(location=R.dot((np.sqrt(3)*a/2,-3*a/2,0)), size = .3, ring_count = 48, segments = 48)
+bpy.ops.mesh.primitive_uv_sphere_add(location=R.dot((np.sqrt(3)*a/2,-3*a/2,0)), radius = .3, ring_count = 48, segments = 48)
 sphere=bpy.context.object
 bpy.context.active_object.name = 'Carbon Atom C Lower'
 sphere.data.materials.append(mat)
 bpy.data.objects['Carbon Atom C Lower'].parent = bpy.data.objects['graph_lower']
-bpy.ops.mesh.primitive_uv_sphere_add(location=R.dot((np.sqrt(3)*a,-a,0)), size = .3, ring_count = 48, segments = 48)
+bpy.ops.mesh.primitive_uv_sphere_add(location=R.dot((np.sqrt(3)*a,-a,0)), radius = .3, ring_count = 48, segments = 48)
 sphere=bpy.context.object
 bpy.context.active_object.name = 'Carbon Atom D Lower'
 sphere.data.materials.append(mat)
 bpy.data.objects['Carbon Atom D Lower'].parent = bpy.data.objects['graph_lower']
-bpy.ops.mesh.primitive_uv_sphere_add(location=R.dot((np.sqrt(3)*a,0,0)), size = .3, ring_count = 48, segments = 48)
+bpy.ops.mesh.primitive_uv_sphere_add(location=R.dot((np.sqrt(3)*a,0,0)), radius = .3, ring_count = 48, segments = 48)
 sphere=bpy.context.object
 bpy.context.active_object.name = 'Carbon Atom E Lower'
 sphere.data.materials.append(mat)
 bpy.data.objects['Carbon Atom E Lower'].parent = bpy.data.objects['graph_lower']
-bpy.ops.mesh.primitive_uv_sphere_add(location=R.dot((np.sqrt(3)*a/2,a/2,0)), size = .3, ring_count = 48, segments = 48)
+bpy.ops.mesh.primitive_uv_sphere_add(location=R.dot((np.sqrt(3)*a/2,a/2,0)), radius = .3, ring_count = 48, segments = 48)
 sphere=bpy.context.object
 bpy.context.active_object.name = 'Carbon Atom F Lower'
 sphere.data.materials.append(mat)
@@ -166,17 +166,17 @@ bpy.data.objects['Bond 4 Lower'].parent = bpy.data.objects['graph_lower']
 bpy.data.objects['Bond 5 Lower'].parent = bpy.data.objects['graph_lower']
 bpy.data.objects['Bond 6 Lower'].parent = bpy.data.objects['graph_lower']
 
-bpy.data.objects['graph_lower'].dupli_type = 'VERTS'
+bpy.data.objects['graph_lower'].instance_type = 'VERTS'
 
 ##########
 
 bn_mat=bpy.data.materials.new('Boron Nitride')
 #bn_mat.diffuse_color=(0,.66,.87)
-bn_mat.diffuse_color=(0,.4,1)
+bn_mat.diffuse_color=(0,.4,1,1)
 bpy.ops.mesh.primitive_cube_add(location=(-4*a,8*a,-1*a))
 bn=bpy.context.object
 bpy.context.active_object.name = 'BN'
-bpy.context.scene.objects.active.scale = (20*a, 20*a, 0.5*a)
+bpy.context.view_layer.objects.active.scale = (20*a, 20*a, 0.5*a)
 bn.data.materials.append(bn_mat)
 #bn.rotation_euler = (0, 0, 1)
 
@@ -184,11 +184,11 @@ bn.data.materials.append(bn_mat)
 
 au_mat=bpy.data.materials.new('Gold')
 #au_mat.diffuse_color=(.98,.74,.64)
-au_mat.diffuse_color=(1,.75,.25)
+au_mat.diffuse_color=(1,.75,.25,1)
 bpy.ops.mesh.primitive_cube_add()
 gold=bpy.context.object
 bpy.context.active_object.name = 'Au'
-bpy.context.scene.objects.active.scale = (8*a, 8*a, 0.5*a)
+bpy.context.view_layer.objects.active.scale = (8*a, 8*a, 0.5*a)
 gold.data.materials.append(au_mat)
 gold.location=(17.95*a,-11.95*a,-1.4*a)
 
@@ -197,22 +197,22 @@ gold.location=(17.95*a,-11.95*a,-1.4*a)
 sio2_mat=bpy.data.materials.new('Silicon Oxide')
 #sio2_mat.diffuse_color=(0.55,.44,0.69)
 #sio2_mat.diffuse_color=(0.49,.32,0.72)
-sio2_mat.diffuse_color=(0.42,.24,0.67)
+sio2_mat.diffuse_color=(0.42,.24,0.67,1)
 bpy.ops.mesh.primitive_cube_add()
 sio2=bpy.context.object
 bpy.context.active_object.name = 'SiO2'
-bpy.context.scene.objects.active.scale = (30*a,30*a, 2*a)
+bpy.context.view_layer.objects.active.scale = (30*a,30*a, 2*a)
 sio2.data.materials.append(sio2_mat)
 sio2.location = (-4*a,10*a,-3*a)
 
 ##########
 
 si_mat=bpy.data.materials.new('Silicon')
-si_mat.diffuse_color=(0.5,0.5,0.5)
+si_mat.diffuse_color=(0.5,0.5,0.5,1)
 bpy.ops.mesh.primitive_cube_add()
 si=bpy.context.object
 bpy.context.active_object.name = 'Si'
-bpy.context.scene.objects.active.scale = (30*a,30*a, 4*a)
+bpy.context.view_layer.objects.active.scale = (30*a,30*a, 4*a)
 si.data.materials.append(si_mat)
 si.location = (-4*a,10*a,-9*a)
 
@@ -224,6 +224,6 @@ w_mat.specular_color=(0.5,0.5,0.5)
 bpy.ops.mesh.primitive_cone_add(vertices = 128)
 tip=bpy.context.object
 bpy.context.active_object.name = 'Tip'
-bpy.context.scene.objects.active.scale = (5*a,5*a, -10*a)
+bpy.context.view_layer.objects.active.scale = (5*a,5*a, -10*a)
 tip.data.materials.append(w_mat)
 tip.location = (5*a,-5*a,15*a)
